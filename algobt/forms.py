7b6f4d8bf2be7f5d36acfc981eff407973fbfbbd
Prashanjeet <jeet8halder@gmail.com>
2020-10-25 11:20:22 +0530

    Entry condition ModelForm test#1

--- algobt/forms.py
+++ algobt/forms.py
@@ -1,7 +1,15 @@
-# Defini
+# Definition of forms for algo_bt
 from django.contrib.auth.forms import AuthenticationForm
 from django.utils.translation import ugettext_lazy as _
-
+from .models import (
+    EntryConditionModel,
+    # EntryConditionLevel_1,
+    # EntryConditionLevel_2,
+    # EntryConditionField_3,
+    # TrailConditionModel,
+    # TrailConditionLevel_1,
+    # RiskManagementModel,
+)
 from django import forms
 
 
@@ -20,3 +28,19 @@ class BootstrapAuthenticationForm(AuthenticationForm):
                                    'placeholder': 'Password'
                                })
                                )
+
+
+class EntryConditionForm(forms.ModelForm):
+    class Meta:
+        model = EntryConditionModel
+        fields = ('field_1', 'field_2')
+        labels = {
+            'field_1': 'Choose',
+            'field_2': 'Choose',
+        }
+        widgets = {
+            'field_1': forms.RadioSelect(attrs={
+                'class': 'entry-cond-form-1',
+                'placeholder': 'Choose one',
+            })
+        }
